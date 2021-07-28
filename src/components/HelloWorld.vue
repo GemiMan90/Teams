df9f6947f3272b5e3dd490ee1cafec3fe973342d
GemiMan90 <dtnmg1@gmail.com>
2021-07-28 03:28:43 +0200

    1st

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,134 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+<div>
+  <h1>{{ msg }}</h1>
+  <form >
+      <p>Player name: <input v-model="playerName" type="text"></p>
+      <p>Player role: 
+        <select v-model="playerRole">
+        <option value="Tank">Tank</option>
+        <option value="DPS">DPS</option>
+      </select>
+      </p>
+
+        <div class="form-group">
+          <label for="my-file">Select Image: </label>
+          <input type="file" accept="image/*" @change="previewImage" class="form-control-file" id="my-file">
+    
+          <div class="border p-2 mt-3">
+            <p>Preview Here:</p>
+            <template v-if="preview">
+              <img :src="preview" class="img-fluid" />
+              <p class="mb-0">file name: {{ image.name }}</p>
+              <p class="mb-0">size: {{ image.size/1024 }}KB</p>
+               </template>
+          </div>
+        </div>
+            <button @click="addTeamOne">Add player to team 1</button>
+            <button @click="addTeamTwo">Add player to team 2</button>
+            <button @click="saveTeams">Register teams</button>
+    </form> 
   </div>
+
+<div class='display' id="display">
+    <p> Player name: {{playerName}}</p>
+    <p> Player role: {{playerRole}}</p>
+</div>
+{{team1}}
+{{team2}}
+<p>
+{{teams}}
+</p>
+
+
+
 </template>
 
 <script>
+//export let team1=[];
+//export let team2=[];
+const axios = require('axios')
+
+
+
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
-  }
+    msg: String,
+  },
+  data () {
+    return{
+    playerName:'Gemi',
+    playerRole:"Tank",
+    photo:"",
+    preview: null,
+    image: null,
+    team1: [],
+    team2: [],
+    teams: {"teamOne":null,"teamTwo":null}
+  
+    }
+  },
+
+  methods: {
+    addTeamOne(e) {
+      console.log(this.playerName,this.playerRole,this.preview);
+      //console.log(e)
+   
+      this.team1.push({name:this.playerName,role:this.playerRole,photo:this.preview})
+      this.teams.teamOne = this.team1
+      console.log(this.team1)
+      this.$emit("addToTeamOne",this.team1)
+    },
+    addTeamTwo(e) {
+      console.log(this.playerName,this.playerRole);
+      //console.log(e)
+
+      this.team2.push({name:this.playerName,role:this.playerRole,photo:this.image})
+      this.teams.teamTwo = this.team2
+      console.log(this.team2)
+      this.$emit("addToTeamTwo",this.team2)
+   
+      //team1.push({name:this.playerName,role:this.playerRole,photo:this.image})
+      //console.log(team1)
+    },
+
+    saveTeams() {
+    axios
+    .post('http://localhost:3000/teams', {teams:this.teams
+      })
+    .then(res => {
+        console.log(`statusCode: ${res.statusCode}`)
+        console.log(res)
+      })
+    .catch(error => {
+        console.error(error)
+      })
+
+    },
+
+    uploadPhoto() {
+      this.photo = this.$refs.photo.files[0]
+    },
+    previewImage: function(event) {
+      var input = event.target;
+      if (input.files) {
+        var reader = new FileReader();
+        reader.onload = (e) => {
+          this.preview = e.target.result;
+        }
+        this.image=input.files[0];
+        reader.readAsDataURL(input.files[0]);
+      }
+    },
+   
+    reset: function() {
+      this.image = null;
+      this.preview = null;
+      this.image_list = [];
+      this.preview_list = [];
+    },
+  },
+
 }
 </script>
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,82 @@
 <template>
+  
   <div class="about">
-    <h1>This is an about page</h1>
+    
+    <div v-for="team in teams" >
+      <!-- <h2 > Players in team {{team}} :</h2> -->
+      <div v-for="player in team">
+        {{Object.keys(team)}}
+        
+
+        <div v-for="single in player">
+          <p>
+          {{Object.keys(team)[0]}}  
+          Player name : {{single.name}}<br>
+          Player role : {{single.role}}<br>
+          Player photo : {{single.photo}}<br>
+          </p>
+        </div>
+      </div>
+
+           
+    </div>
+
+
+
   </div>
 </template>
+
+<script>
+
+import HelloWorld from '@/components/HelloWorld.vue'
+import team1 from '@/components/HelloWorld.vue';
+import team2 from '@/components/HelloWorld.vue';
+
+
+export default {
+  name: 'Home',
+  
+  components: {
+    HelloWorld
+  },
+
+  //props:['team1','team2'],
+
+  data () {
+    return {
+      //player:{name:"",role:""},
+      //playerTwo:{name:"",role:""},
+
+
+
+      teams:{},
+      Pindex:0
+    }
+  },
+  mounted() {
+    fetch("http://localhost:3000/teams")
+    .then((res)=>res.json())
+    .then(Data => this.teams = Data)
+    .then(Data => console.log(Data))    
+    .catch(err => console.log(err.message))
+    
+  },
+
+  methods: {
+    AddPlayer(player){
+      player.name=this.name,
+      player.role=this.role,
+      team1.push(player)
+      console.log(player)
+    },
+    updateTeamOne(Team1){
+      this.team1 = Team1
+    },
+    updateTeamTwo(Team2){
+      this.team2 = Team2
+    },
+
+  }
+  
+}
+</script>
